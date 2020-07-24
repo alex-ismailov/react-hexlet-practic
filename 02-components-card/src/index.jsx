d131467dc6dba9f39b546f5c94ae2df2e8867158
smlnx <alexander.is.dev@gmail.com>
2020-07-24 09:27:36 +0300

    implement 02-components-card/ - ex.

---
+++ 02-components-card/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+// BEGIN (write your solution here)
+import Card from './Card.jsx';
+
+const mount = document.getElementById('container');
+ReactDOM.render(<Card />, mount);
+// END
