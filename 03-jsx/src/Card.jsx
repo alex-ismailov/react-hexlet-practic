7ad0dea8feac4a051066262cf99605f00a64fcef
smilenb <alexander.is.dev@gmail.com>
2020-07-26 17:46:03 +0300

    refactor(03-jsx/src/Card.jsx) params and remove comments

--- 03-jsx/src/Card.jsx
+++ 03-jsx/src/Card.jsx
@@ -2,22 +2,14 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-export default (dataObj) => {
-  const { title, text } = dataObj;
+export default ({ title, text }) => {
   if (!title && !text) {
     return null;
   }
 
-  // return (
-  //   <div className="card">
-  //     <div className="card-body">
-  //       {title ? <h4 className="card-title">{title}</h4> : null}
-  //       {text ? <p className="card-text">{text}</p> : null}
-  //     </div>
-  //   </div>
-  // );
   const titleDom = title && <h4 className="card-title">{title}</h4>;
   const textDom = text && <p className="card-text">{text}</p>;
+
   return (
     <div className="card">
       <div className="card-body">
