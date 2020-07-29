131b25d86a1728513a1ee75edee2a37ccf6ac74a
smlnx <alexander.is.dev@gmail.com>
2020-07-29 13:50:18 +0300

    solve 07-handling-class-names ex.

---
+++ 07-handling-class-names/src/index.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from 'react-dom';
+import React from 'react';
+
+import Alert from './Alert.jsx';
+
+ReactDOM.render(
+  <Alert type="warning" text="what is love?" />,
+  document.getElementById('container'),
+);
