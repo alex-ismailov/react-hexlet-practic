12bb4a9982efb7a5c04a2aca5927757bb8b211c2
smlnx <alexander.is.dev@gmail.com>
2020-07-29 18:43:18 +0300

    refactor(08-children/src/) ListGroup.jsx

--- 08-children/src/ListGroup.jsx
+++ 08-children/src/ListGroup.jsx
@@ -6,14 +6,12 @@ import React from 'react';
 export default class ListGroup extends React.Component {
   render() {
     const { children } = this.props;
-    const processedChildren = React.Children.map(children, (child) => {
-      return (
-        <li className='list-group-item'>{child}</li>
-      );
-    });
+    const processedChildren = React.Children.map(children, (child) => (
+      <li className="list-group-item">{child}</li>
+    ));
 
     return (
-      <ul className='list-group'>
+      <ul className="list-group">
         {processedChildren}
       </ul>
     );
