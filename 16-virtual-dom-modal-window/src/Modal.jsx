5009c668d106cf2e12960a9cb23a82576035c643
smlnx <alexander.is.dev@gmail.com>
2020-08-04 21:47:21 +0300

    refactor(16-virtual-dom-modal-window/src/) Modal.jsx

--- 16-virtual-dom-modal-window/src/Modal.jsx
+++ 16-virtual-dom-modal-window/src/Modal.jsx
@@ -4,9 +4,9 @@ import cn from 'classnames';
 import React from 'react';
 
 // BEGIN (write your solution here)
-const Header = ({ toggle }) => (
+const Header = ({ toggle, children }) => (
   <div className="modal-header">
-    <div className="modal-title">Modal title</div>
+    <div className="modal-title">{children}</div>
     <button onClick={toggle} type="button" className="close" data-dismiss="modal" aria-label="Close">
       <span aria-hidden="true">×</span>
     </button>
@@ -18,6 +18,10 @@ const Body = ({ children }) => <p className="modal-body">{children}</p>;
 const Footer = ({ children }) => <p className="modal-footer">{children}</p>;
 
 export default class Modal extends React.Component {
+  static defaultProps = {
+    isOpen: false,
+  };
+
   static Header = Header;
 
   static Body = Body;
