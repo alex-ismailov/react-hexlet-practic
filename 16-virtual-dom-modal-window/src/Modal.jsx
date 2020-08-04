c0f4a372a55b07624fc9bff352db77aabde70ccd
smlnx <alexander.is.dev@gmail.com>
2020-08-04 21:36:40 +0300

    implement 16-virtual-dom-modal-window/ - ex.

---
+++ 16-virtual-dom-modal-window/src/Modal.jsx
@@ -0,0 +1,49 @@
+/* eslint-disable react/static-property-placement */
+
+import cn from 'classnames';
+import React from 'react';
+
+// BEGIN (write your solution here)
+const Header = ({ toggle }) => (
+  <div className="modal-header">
+    <div className="modal-title">Modal title</div>
+    <button onClick={toggle} type="button" className="close" data-dismiss="modal" aria-label="Close">
+      <span aria-hidden="true">×</span>
+    </button>
+  </div>
+);
+
+const Body = ({ children }) => <p className="modal-body">{children}</p>;
+
+const Footer = ({ children }) => <p className="modal-footer">{children}</p>;
+
+export default class Modal extends React.Component {
+  static Header = Header;
+
+  static Body = Body;
+
+  static Footer = Footer;
+
+  render() {
+    const { isOpen, children } = this.props;
+    const modalClass = cn({
+      modal: true,
+      fade: isOpen,
+      show: isOpen,
+    });
+
+    return (
+      <div
+        className={modalClass}
+        style={{ display: isOpen ? 'block' : 'none' }}
+      >
+        <div className="modal-dialog" role="document">
+          <div className="modal-content">
+            {children}
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+// END
