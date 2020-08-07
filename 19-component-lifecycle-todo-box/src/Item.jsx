abdd99d0d276dd0055f8f30837594400469fdb4a
smlnx <alexander.is.dev@gmail.com>
2020-08-07 23:51:10 +0300

    refactor(19-component-lifecycle-todo-box/src/) Item.jsx

--- 19-component-lifecycle-todo-box/src/Item.jsx
+++ 19-component-lifecycle-todo-box/src/Item.jsx
@@ -3,5 +3,15 @@
 import React from 'react';
 
 // BEGIN (write your solution here)
-
+export default (props) => {
+  const { id, body, onClick} = props;
+  return (
+    <div className="row">
+      <div className="col-1">{id}</div>
+      <div className="col">
+        <a onClick={onClick} href="#" className="todo-task">{body}</a>
+      </div>
+    </div>
+  );
+};
 // END
