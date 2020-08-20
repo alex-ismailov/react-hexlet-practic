5483b52025bbbda715859250b261b0f6c8d13ed7
smlnx <alexander.is.dev@gmail.com>
2020-08-20 21:51:11 +0300

    delete(redux/06-react-redux-tasklist/src/components/App.jsx) comments

--- redux/06-react-redux-tasklist/src/components/App.jsx
+++ redux/06-react-redux-tasklist/src/components/App.jsx
@@ -4,14 +4,6 @@ import _ from 'lodash';
 import { updateNewTaskText, addTask, removeTask } from '../actions/index';
 
 // BEGIN (write your solution here)
-// const mapStateToProps = (state) => {
-//   const props = {
-//     text: state.text,
-//     tasks: state.tasks,
-//   };
-//   return props;
-// };
-
 const mapStateToProps = ({ text, tasks }) => {
   const props = { text, tasks };
   return props;
