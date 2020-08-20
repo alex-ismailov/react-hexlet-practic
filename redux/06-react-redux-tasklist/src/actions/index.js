e04a82cb41d0e4397ca21d55abf931e682e96ce0
smlnx <alexander.is.dev@gmail.com>
2020-08-20 21:12:13 +0300

    fix(redux/06-react-redux-tasklist/src/actions/index.js) eslint errors

--- redux/06-react-redux-tasklist/src/actions/index.js
+++ redux/06-react-redux-tasklist/src/actions/index.js
@@ -10,13 +10,13 @@ export const addTask = (task) => ({
   type: 'TASK_ADD',
   payload: {
     task,
-  } 
+  },
 });
 
 export const removeTask = (id) => ({
   type: 'TASK_REMOVE',
   payload: {
     id,
-  }
+  },
 });
 // END
