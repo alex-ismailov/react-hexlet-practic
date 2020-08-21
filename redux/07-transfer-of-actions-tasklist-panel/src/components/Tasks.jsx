3a2110747146762f0d6ba16fa8d0082a7f7a55c6
smlnx <alexander.is.dev@gmail.com>
2020-08-21 22:29:57 +0300

    implement(redux/07-transfer-of-actions-tasklist-panel/src/components/) Tasks.jsx

--- redux/07-transfer-of-actions-tasklist-panel/src/components/Tasks.jsx
+++ redux/07-transfer-of-actions-tasklist-panel/src/components/Tasks.jsx
@@ -1,3 +1,35 @@
 // BEGIN (write your solution here)
+import React from 'react';
+import { connect } from 'react-redux';
 
+const mapStateToProps = (state) => {
+  const props = {
+    tasks: state.tasks,
+  };
+  return props;
+};
+
+class Tasks extends React.Component {
+  render() {
+    const { tasks } = this.props;
+    if (tasks.length === 0) {
+      return null;
+    }
+    
+    return (
+      <div className="mt-3">
+        <ul className="list-group">
+          {tasks.map((task) => (
+            <li key={task.id} className="list-group-item d-flex">
+              <span className="mr-auto">{task.text}</span>
+              <button type="button" className="close"><span>×</span></button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps)(Tasks);
 // END
