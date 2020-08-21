87da8f577c110487c03c044f15cc86c293c7b97b
smlnx <alexander.is.dev@gmail.com>
2020-08-21 22:28:21 +0300

    implement(redux/07-transfer-of-actions-tasklist-panel/src/components/Panel.jsx) buttons and handleTasksClean function

--- redux/07-transfer-of-actions-tasklist-panel/src/components/Panel.jsx
+++ redux/07-transfer-of-actions-tasklist-panel/src/components/Panel.jsx
@@ -5,5 +5,38 @@ import faker from '../faker.js';
 import * as actions from '../actions/index.js';
 
 // BEGIN (write your solution here)
+const mapStateToProps = (state) => {
+  const props = {
+    tasks: state.tasks,
+  };
+  return props;
+};
 
+const actionCreators = {
+  generateTasks: actions.generateTasks,
+  cleanTasks: actions.cleanTasks,
+};
+
+class Panel extends React.Component {
+  handleTasksGenerate = () => {
+
+  };
+
+  handleTasksClean = () => {
+    const { cleanTasks } = this.props;
+    cleanTasks();
+  };
+
+  render() {
+
+    return (
+      <div className="py-3">
+        <button onClick={this.handleTasksClean} type="button" data-test="clean" className="btn btn-warning btn-sm mr-3">Clean</button>
+        <button onClick={this.handleTasksGenerate} type="button" data-test="generate" className="btn btn-primary btn-sm">Generate</button>
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, actionCreators)(Panel);
 // END
