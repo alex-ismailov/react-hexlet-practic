b87f3f8c31c04962181f85a406040ded2d199d61
smlnx <alexander.is.dev@gmail.com>
2020-08-21 21:02:27 +0300

    init(redux/) 07-transfer-of-actions-tasklist-panel/ - ex.

---
+++ redux/07-transfer-of-actions-tasklist-panel/src/components/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import NewTaskForm from './NewTaskForm.jsx';
+import Panel from './Panel.jsx';
+import Tasks from './Tasks.jsx';
+
+const App = () => (
+  <div className="col-5">
+    <NewTaskForm />
+    <Panel />
+    <Tasks />
+  </div>
+);
+
+export default App;
