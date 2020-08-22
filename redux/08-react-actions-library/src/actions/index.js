90d6dd74ba148b48620ae70c700eaeb3cef349c1
smlnx <alexander.is.dev@gmail.com>
2020-08-23 01:24:58 +0300

    implement(redux/08-react-actions-library/src/actions/index.js) actions

--- redux/08-react-actions-library/src/actions/index.js
+++ redux/08-react-actions-library/src/actions/index.js
@@ -3,5 +3,7 @@ import { createAction } from 'redux-actions';
 export const updateNewTaskText = createAction('TEXT_UPDATE');
 
 // BEGIN (write your solution here)
-
+export const addTask = createAction('TASK_ADD');
+export const removeTask = createAction('TASK_REMOVE');
+export const cleanTasks = createAction('TASK_CLEAN');
 // END
