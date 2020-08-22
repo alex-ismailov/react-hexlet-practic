2cf40fc1e4c51ceac024feaf4974fdca9f19efc4
smlnx <alexander.is.dev@gmail.com>
2020-08-23 01:27:16 +0300

    implement(redux/08-react-actions-library/src/reducers/index.js) taskText, tasks reducers

--- redux/08-react-actions-library/src/reducers/index.js
+++ redux/08-react-actions-library/src/reducers/index.js
@@ -3,7 +3,34 @@ import { handleActions } from 'redux-actions';
 import * as actions from '../actions/index.js';
 
 // BEGIN (write your solution here)
+const {
+  updateNewTaskText,
+  addTask,
+} = actions;
 
+const defaultState = {
+  text: '',
+  tasks: [],
+};
+const taskText = handleActions(
+  {
+    [updateNewTaskText]: (state, { payload: text }) => {
+      return text;
+    },
+    [addTask]: () => {
+      return '';
+    }
+  },
+  defaultState.text,
+);
+const tasks = handleActions(
+  {
+    [addTask]: (state, { payload: task }) => {
+      return [ task, ...state ];
+    }
+  },
+  defaultState.tasks,
+);
 // END
 
 export default combineReducers({
