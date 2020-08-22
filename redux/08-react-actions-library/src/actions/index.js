27a65adb3798da075b2d1375be26526ef4799362
smlnx <alexander.is.dev@gmail.com>
2020-08-23 01:41:14 +0300

    remove(redux/08-react-actions-library/src/actions/index.js) cleanTasks action

--- redux/08-react-actions-library/src/actions/index.js
+++ redux/08-react-actions-library/src/actions/index.js
@@ -5,5 +5,4 @@ export const updateNewTaskText = createAction('TEXT_UPDATE');
 // BEGIN (write your solution here)
 export const addTask = createAction('TASK_ADD');
 export const removeTask = createAction('TASK_REMOVE');
-export const cleanTasks = createAction('TASK_CLEAN');
 // END
