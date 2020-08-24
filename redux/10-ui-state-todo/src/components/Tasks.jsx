7234ba90a6e5cbc24744a0f11014f73eb809cd40
smlnx <alexander.is.dev@gmail.com>
2020-08-25 01:53:04 +0300

    implement(redux/10-ui-state-todo/src/components/Tasks.jsx) work in browser, but not pass tests

--- redux/10-ui-state-todo/src/components/Tasks.jsx
+++ redux/10-ui-state-todo/src/components/Tasks.jsx
@@ -5,7 +5,14 @@ import * as actions from '../actions/index.js';
 
 const mapStateToProps = (state) => {
   // BEGIN (write your solution here)
-  
+  const {
+    tasks: { byId, allIds },
+    tasksUIState,
+  } = state;
+
+  const tasks = allIds.map((id) => byId[id]);
+
+  return { tasks, tasksUIState };
   // END
 };
 
@@ -15,7 +22,52 @@ const actionCreators = {
 
 class Tasks extends React.Component {
   // BEGIN (write your solution here)
-  
+  handleInverseTaskTheme = (task) => (e) => {
+    e.preventDefault();
+    const { inverseTaskTheme } = this.props;
+    inverseTaskTheme({ task });
+  };
+
+  render() {
+    const { tasksUIState } = this.props;
+    console.log(tasksUIState);
+
+    // TODO bg-light text-dark <=> bg-dark text-light
+    const { tasks } = this.props;
+
+    const commonClasses = `list-group-item d-flex`;
+    const lightClasses = cn(commonClasses, {
+      'bg-light': true,
+      'text-dark': true,
+    });
+    const darkClasses = cn(commonClasses, {
+      'bg-dark': true,
+      'text-light': true,
+    });
+
+    return (
+      <div className="mt-3">
+        <ul className="list-group">
+          {tasks.map((task) => (
+            <li
+              key={task.id}
+              className={tasksUIState[task.id].theme === 'light' ? lightClasses : darkClasses}
+            >
+              <span className="mr-auto">
+                <a
+                  onClick={this.handleInverseTaskTheme(task)}
+                  href="#"
+                  className="text-reset"
+                >
+                  {task.text}
+                </a>
+              </span>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
   // END
 }
 
