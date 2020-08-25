e8e1456e1cfc81bfc6c6338327e1cfeeb16ad0b9
smlnx <alexander.is.dev@gmail.com>
2020-08-25 22:11:24 +0300

    refactor(redux/10-ui-state-todo/src/components/) Tasks.jsx

--- redux/10-ui-state-todo/src/components/Tasks.jsx
+++ redux/10-ui-state-todo/src/components/Tasks.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+
 import React from 'react';
 import { connect } from 'react-redux';
 import cn from 'classnames';
@@ -28,42 +30,44 @@ class Tasks extends React.Component {
     inverseTaskTheme({ task });
   };
 
+  renderTask(task) {
+    const { tasksUIState } = this.props;
+
+    const themeToClasses = {
+      light: 'bg-light text-dark',
+      dark: 'bg-dark text-light'
+    };
+    const currentThemeClass = themeToClasses[tasksUIState[task.id].theme];
+    const classes = cn({
+      'list-group-item d-flex': true,
+      [currentThemeClass]: true,
+    });
+
+    return (
+      <li key={task.id} className={classes}>
+        <span className="mr-auto">
+          <a
+            onClick={this.handleInverseTaskTheme(task)}
+            href="#"
+            className="text-reset"
+          >
+            {task.text}
+          </a>
+        </span>
+      </li>
+    );
+  }
+
   render() {
     const { tasks } = this.props;
     if (tasks.length === 0) {
       return null;
     }
 
-    const { tasksUIState } = this.props;
-    const commonClasses = 'list-group-item d-flex';
-    const lightClasses = cn(commonClasses, {
-      'bg-light': true,
-      'text-dark': true,
-    });
-    const darkClasses = cn(commonClasses, {
-      'bg-dark': true,
-      'text-light': true,
-    });
-
     return (
       <div className="mt-3">
         <ul className="list-group">
-          {tasks.map((task) => (
-            <li
-              key={task.id}
-              className={tasksUIState[task.id].theme === 'light' ? lightClasses : darkClasses}
-            >
-              <span className="mr-auto">
-                <a
-                  onClick={this.handleInverseTaskTheme(task)}
-                  href="#"
-                  className="text-reset"
-                >
-                  {task.text}
-                </a>
-              </span>
-            </li>
-          ))}
+          {tasks.map((task) => (this.renderTask(task)))}
         </ul>
       </div>
     );
