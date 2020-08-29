26b9adc4ed373ece21bc5d51f90c1c8b865608db
smlnx <alexander.is.dev@gmail.com>
2020-08-29 21:30:37 +0300

    add(redux/11-reselect-library-filter-tasks/src/__tests__/test.jsx) comments

--- redux/11-reselect-library-filter-tasks/src/__tests__/test.jsx
+++ redux/11-reselect-library-filter-tasks/src/__tests__/test.jsx
@@ -17,42 +17,42 @@ test('Store', () => {
     </Provider>
   );
   const wrapper = mount(vdom);
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 1
 
   const newTaskInput = wrapper.find('input[type="text"]');
   const newTaskSubmit = wrapper.find('input[type="submit"]');
 
   newTaskInput.simulate('change', { target: { value: 'na-na' } });
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 2
 
   newTaskSubmit.simulate('submit');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 3
 
   newTaskInput.simulate('change', { target: { value: 'another task' } });
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 4
 
   newTaskSubmit.simulate('submit');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 5
 
   const link = wrapper.find('[data-test="task-toggle-state"]').first();
   link.simulate('click');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 6
 
   const activeFilterButton = wrapper.find('[data-test="task-filter-active"]');
   const finishedFilterButton = wrapper.find('[data-test="task-filter-finished"]');
 
   activeFilterButton.simulate('click');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 7
 
   finishedFilterButton.simulate('click');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 8
 
   const allFilterButton = wrapper.find('[data-test="task-filter-all"]');
 
   allFilterButton.simulate('click');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 9
 
   const links = wrapper.find('[data-test="task-remove"]');
   links.last().simulate('click');
-  expect(wrapper.render()).toMatchSnapshot();
+  expect(wrapper.render()).toMatchSnapshot(); // Store 10
 });
