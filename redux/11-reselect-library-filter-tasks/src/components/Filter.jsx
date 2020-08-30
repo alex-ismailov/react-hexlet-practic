2cd73e0dd85f603aa5205f031ece08731c5b307b
smlnx <alexander.is.dev@gmail.com>
2020-08-30 23:08:54 +0300

    refactor(redux/11-reselect-library-filter-tasks/src/components/) Filter.jsx

--- redux/11-reselect-library-filter-tasks/src/components/Filter.jsx
+++ redux/11-reselect-library-filter-tasks/src/components/Filter.jsx
@@ -5,5 +5,49 @@ import * as actions from '../actions/index.js';
 const filters = [['all', 'All Tasks'], ['active', 'Active Tasks'], ['finished', 'Finished Tasks']];
 
 // BEGIN (write your solution here)
+const mapStateToProps = (state) => {
+  const { tasks: { currentFilterName } } = state;
 
+  return { currentFilterName };
+};
+
+const actionCreators = {
+  setTasksFilter: actions.setTasksFilter,
+};
+
+class Filter extends React.Component {
+
+  handleFilter = (e) => {
+    e.preventDefault();
+    
+  };
+
+  render() {
+    const { currentFilterName } = this.props;
+
+    return (
+      <div className="mt-3 d-flex justify-content-around">
+        {filters.map((filter) => {
+          const [ filterState, filterTitle ] = filter;
+          if (filterState === currentFilterName) {
+            return filterTitle;
+          }
+          return (
+            <button
+              onClick={this.handleFilter}
+              key={filterState}
+              type="button"
+              className="btn btn-link border-0 p-0"
+              data-test="task-filter-active"
+            >
+              {filterTitle}
+            </button>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default connect(mapStateToProps, actionCreators)(Filter);
 // END
