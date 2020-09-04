8c791de82206f63779f09f7667f893305d25bd6b
smilenb <alexander.is.dev@gmail.com>
2020-09-04 18:35:28 +0300

    refactor(redux/11-reselect-library-filter-tasks/src/selectors/) index.js

--- redux/11-reselect-library-filter-tasks/src/selectors/index.js
+++ redux/11-reselect-library-filter-tasks/src/selectors/index.js
@@ -1,11 +1,14 @@
 import { createSelector } from 'reselect';
 
 // BEGIN (write your solution here)
-const getTasks = (state) => state.tasks;
+const getTasks = (state) => {
+  return state.tasks;
+};
 
 export const filteredTasksSelector = createSelector(
   getTasks,
   (tasks) => {
+    console.log('selector work');
     const { byId, allIds } = tasks;
     return allIds.map((id) => byId[id]);
   },
