22a0767f0a598312db461939ea2f5890f9b939d4
smlnx <alexander.is.dev@gmail.com>
2020-09-08 22:51:43 +0300

    implement(redux/11-reselect-library-filter-tasks/src/selectors/) index.js, all test pass

--- redux/11-reselect-library-filter-tasks/src/selectors/index.js
+++ redux/11-reselect-library-filter-tasks/src/selectors/index.js
@@ -1,40 +1,19 @@
 import { createSelector } from 'reselect';
 
 // BEGIN (write your solution here)
-const getTasks = (state) => {
-  return state.tasks;
-};
-
-export const allTasksSelector = createSelector(
-  getTasks,
-  (tasks) => {
-    const { byId, allIds } = tasks;
-    return allIds.map((id) => byId[id])
-  },
-);
-
-export const activeTasksSelector = createSelector(
-  allTasksSelector,
-  (tasks) => {
-    const res = tasks.filter((task) => task.state === 'active');
-    console.log(res);
-    return res;
-  },
-);
-
-export const finishedTasksSelector = createSelector(
-  allTasksSelector,
-  (tasks) => {
-    const res = tasks.filter((task) => task.state === 'finished');
-    console.log(res);
-    return res;
-  },
-);
+const getTasks = (state) => state.tasks;
 
 export const filteredTasksSelector = createSelector(
-  allTasksSelector,
-  activeTasksSelector,
-  finishedTasksSelector,
-  (tasks) => tasks,
+  getTasks,
+  (tasks) => {
+    const { currentFilterName } = tasks;
+    const { byId, allIds } = tasks;
+    const mappedTasks = allIds.map((id) => byId[id]);
+    if (currentFilterName === 'all') {
+      return mappedTasks;
+    }
+    
+    return mappedTasks.filter((task) => task.state === currentFilterName);
+  },
 );
 // END
