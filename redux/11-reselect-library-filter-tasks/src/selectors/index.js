3edba34d125a6e652bc81b18db7f0b01ecb2bffe
smlnx <alexander.is.dev@gmail.com>
2020-09-08 22:14:03 +0300

    add(redux/11-reselect-library-filter-tasks/src/selectors/index.js) allTasksSelector activeTasksSelector

--- redux/11-reselect-library-filter-tasks/src/selectors/index.js
+++ redux/11-reselect-library-filter-tasks/src/selectors/index.js
@@ -7,19 +7,34 @@ const getTasks = (state) => {
 
 export const allTasksSelector = createSelector(
   getTasks,
-  (tasks) => tasks.allIds.map((id) => byId[id]),
+  (tasks) => {
+    const { byId, allIds } = tasks;
+    return allIds.map((id) => byId[id])
+  },
 );
 
-export const activeTasksSelector = createSelector();
+export const activeTasksSelector = createSelector(
+  allTasksSelector,
+  (tasks) => {
+    const res = tasks.filter((task) => task.state === 'active');
+    console.log(res);
+    return res;
+  },
+);
 
-export const finishedTasksSelector = createSelector();
+export const finishedTasksSelector = createSelector(
+  allTasksSelector,
+  (tasks) => {
+    const res = tasks.filter((task) => task.state === 'finished');
+    console.log(res);
+    return res;
+  },
+);
 
 export const filteredTasksSelector = createSelector(
-  getTasks,
-  (tasks) => {
-    console.log('selector work');
-    const { byId, allIds } = tasks;
-    return allIds.map((id) => byId[id]);
-  },
+  allTasksSelector,
+  activeTasksSelector,
+  finishedTasksSelector,
+  (tasks) => tasks,
 );
 // END
