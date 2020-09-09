781e3f89cd53bbdee941d6a2594dd737a278cdd5
smilenb <alexander.is.dev@gmail.com>
2020-09-10 00:09:37 +0300

    reinit(redux/) 12-redux-form-lib - ex.

---
+++ redux/12-redux-form-lib/src/components/NewTaskForm.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import _ from 'lodash';
+import { connect } from 'react-redux';
+import { Field, reduxForm } from 'redux-form';
+import * as actions from '../actions/index.js';
+
+const mapStateToProps = () => {
+  const props = {};
+  return props;
+};
+
+const actionCreators = {
+  addTask: actions.addTask,
+};
+
+class NewTaskForm extends React.Component {
+  // BEGIN (write your solution here)
+  
+  // END
+
+  render() {
+    const { handleSubmit } = this.props;
+    return (
+      <form className="form-inline" onSubmit={handleSubmit(this.handleSubmit)}>
+        <div className="form-group mx-3">
+          <Field name="text" required component="input" type="text" />
+        </div>
+        <input type="submit" className="btn btn-primary btn-sm" value="Add" />
+      </form>
+    );
+  }
+}
+
+const ConnectedNewTaskForm = connect(mapStateToProps, actionCreators)(NewTaskForm);
+// BEGIN (write your solution here)
+
+// END
