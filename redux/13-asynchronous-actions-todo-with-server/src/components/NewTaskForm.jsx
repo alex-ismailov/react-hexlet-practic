2075a6be5a18f228ac8c17ea9f4790ef6844f7d7
smlnx <alexander.is.dev@gmail.com>
2020-09-14 09:06:07 +0300

    implement(redux/13-asynchronous-actions-todo-with-server/src/components/) NewTaskForm.jsx

--- redux/13-asynchronous-actions-todo-with-server/src/components/NewTaskForm.jsx
+++ redux/13-asynchronous-actions-todo-with-server/src/components/NewTaskForm.jsx
@@ -14,15 +14,34 @@ const actionCreators = {
 
 class NewTaskForm extends React.Component {
   // BEGIN (write your solution here)
-  
+  handleSubmit = async (values) => {
+    const { addTask, reset } = this.props;
+    try {
+      await addTask({ task: values });
+    } catch (error) {
+      throw new SubmissionError({ _error: error.message });
+    }
+    reset();
+  }
   // END
 
   render() {
     const {
       handleSubmit, submitting, pristine, error,
     } = this.props;
+
     // BEGIN (write your solution here)
     
+
+    return (
+      <form className="form-inline" onSubmit={handleSubmit(this.handleSubmit)}>
+        <div className="form-group mx-3">
+          <Field name="text" required disabled={submitting} component="input" type="text" />
+        </div>
+        <input type="submit" disabled={pristine || submitting} className="btn btn-primary btn-sm" value="Add" />
+        {error && <div className="ml-3">{error}</div>}
+      </form>
+    );
     // END
   }
 }
